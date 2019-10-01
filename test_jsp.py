ac304ceffa9d9c3d85dfc9a5ff8861aa5d50cef5
Mike Zielewski <zielewski.michael.ryan.q5@dc.tohoku.ac.jp>
2019-10-01 09:50:14 +0900

    Add one_at_a_time tests

--- test_jsp.py
+++ test_jsp.py
@@ -11,3 +11,12 @@ class TestConstraints(unittest.TestCase):
         valid_configs = [[1], [1, 0], [0, 0, 1]]
         for config in valid_configs:
             self.assertTrue(jsp_mod.start_once(*config))
+
+    def test_one_at_a_time(self):
+        invalid_configs = [[1, 1]]
+        for config in invalid_configs:
+            self.assertFalse(jsp_mod.one_at_a_time(*config))
+
+        valid_configs = [[1, 0], [0, 1], [0, 0]]
+        for config in valid_configs:
+            self.assertTrue(jsp_mod.one_at_a_time(*config))
