17b2b25fe0a852bcef675cb8ca4860a19c09394c
Mike Zielewski <zielewski.michael.ryan.q5@dc.tohoku.ac.jp>
2019-10-01 10:50:39 +0900

    Add CSP tests

--- test_jsp.py
+++ test_jsp.py
@@ -29,3 +29,64 @@ class TestConstraints(unittest.TestCase):
         valid_configs = [[1, 0], [0, 1], [0, 0]]
         for config in valid_configs:
             self.assertTrue(jsp_mod.enforce_precedence(*config))
+
+
+class TestCSP(unittest.TestCase):
+    def test_simple_jsp(self):
+        jobs = {
+            "j1": [("m1", 2), ("m2", 1), ("m3", 1)],
+            "j2": [("m3", 2), ("m1", 1), ("m2", 2)],
+            "j3": [("m2", 1), ("m1", 1), ("m3", 2)]
+        }
+        max_time = 7
+
+        jsp = jsp_mod.JobShopScheduler(jobs, max_time, True)
+        csp = jsp.csp
+
+        valid_schedules = [{
+            'x_j1_o1_t1': 0, 'x_j1_o1_t2': 1, 'x_j1_o1_t3': 0, 'x_j1_o1_t4': 0,
+            'x_j1_o2_t3': 0, 'x_j1_o2_t4': 0, 'x_j1_o2_t5': 1, 'x_j1_o2_t6': 0,
+            'x_j1_o3_t4': 0, 'x_j1_o3_t5': 0, 'x_j1_o3_t6': 0, 'x_j1_o3_t7': 1,
+            'x_j2_o1_t1': 1, 'x_j2_o1_t2': 0, 'x_j2_o1_t3': 0,
+            'x_j2_o2_t3': 0, 'x_j2_o2_t4': 0, 'x_j2_o2_t5': 1,
+            'x_j2_o3_t4': 0, 'x_j2_o3_t5': 0, 'x_j2_o3_t6': 1,
+            'x_j3_o1_t1': 0, 'x_j3_o1_t2': 0, 'x_j3_o1_t3': 1, 'x_j3_o1_t4': 0,
+            'x_j3_o2_t2': 0, 'x_j3_o2_t3': 0, 'x_j3_o2_t4': 1, 'x_j3_o2_t5': 0,
+            'x_j3_o3_t3': 0, 'x_j3_o3_t4': 0, 'x_j3_o3_t5': 1, 'x_j3_o3_t6': 0
+            }, {
+            'x_j1_o1_t1': 1, 'x_j1_o1_t2': 0, 'x_j1_o1_t3': 0, 'x_j1_o1_t4': 0,
+            'x_j1_o2_t3': 1, 'x_j1_o2_t4': 0, 'x_j1_o2_t5': 0, 'x_j1_o2_t6': 0,
+            'x_j1_o3_t4': 0, 'x_j1_o3_t5': 0, 'x_j1_o3_t6': 0, 'x_j1_o3_t7': 1,
+            'x_j2_o1_t1': 0, 'x_j2_o1_t2': 0, 'x_j2_o1_t3': 1,
+            'x_j2_o2_t3': 0, 'x_j2_o2_t4': 0, 'x_j2_o2_t5': 1,
+            'x_j2_o3_t4': 0, 'x_j2_o3_t5': 0, 'x_j2_o3_t6': 1,
+            'x_j3_o1_t1': 1, 'x_j3_o1_t2': 0, 'x_j3_o1_t3': 0, 'x_j3_o1_t4': 0,
+            'x_j3_o2_t2': 0, 'x_j3_o2_t3': 0, 'x_j3_o2_t4': 1, 'x_j3_o2_t5': 0,
+            'x_j3_o3_t3': 0, 'x_j3_o3_t4': 0, 'x_j3_o3_t5': 1, 'x_j3_o3_t6': 0
+        }]
+        for schedule in valid_schedules:
+            self.assertTrue(csp.check(schedule))
+
+        invalid_schedules = [{
+            'x_j1_o1_t1': 0, 'x_j1_o1_t2': 1, 'x_j1_o1_t3': 0, 'x_j1_o1_t4': 1,
+            'x_j1_o2_t3': 0, 'x_j1_o2_t4': 0, 'x_j1_o2_t5': 1, 'x_j1_o2_t6': 0,
+            'x_j1_o3_t4': 0, 'x_j1_o3_t5': 0, 'x_j1_o3_t6': 0, 'x_j1_o3_t7': 1,
+            'x_j2_o1_t1': 1, 'x_j2_o1_t2': 0, 'x_j2_o1_t3': 0,
+            'x_j2_o2_t3': 0, 'x_j2_o2_t4': 0, 'x_j2_o2_t5': 1,
+            'x_j2_o3_t4': 0, 'x_j2_o3_t5': 0, 'x_j2_o3_t6': 1,
+            'x_j3_o1_t1': 0, 'x_j3_o1_t2': 0, 'x_j3_o1_t3': 1, 'x_j3_o1_t4': 0,
+            'x_j3_o2_t2': 0, 'x_j3_o2_t3': 0, 'x_j3_o2_t4': 1, 'x_j3_o2_t5': 0,
+            'x_j3_o3_t3': 0, 'x_j3_o3_t4': 0, 'x_j3_o3_t5': 1, 'x_j3_o3_t6': 0
+            }, {
+            'x_j1_o1_t1': 0, 'x_j1_o1_t2': 0, 'x_j1_o1_t3': 0, 'x_j1_o1_t4': 1,
+            'x_j1_o2_t3': 1, 'x_j1_o2_t4': 0, 'x_j1_o2_t5': 0, 'x_j1_o2_t6': 0,
+            'x_j1_o3_t4': 0, 'x_j1_o3_t5': 0, 'x_j1_o3_t6': 0, 'x_j1_o3_t7': 1,
+            'x_j2_o1_t1': 0, 'x_j2_o1_t2': 0, 'x_j2_o1_t3': 1,
+            'x_j2_o2_t3': 0, 'x_j2_o2_t4': 0, 'x_j2_o2_t5': 1,
+            'x_j2_o3_t4': 0, 'x_j2_o3_t5': 0, 'x_j2_o3_t6': 1,
+            'x_j3_o1_t1': 1, 'x_j3_o1_t2': 0, 'x_j3_o1_t3': 0, 'x_j3_o1_t4': 0,
+            'x_j3_o2_t2': 0, 'x_j3_o2_t3': 0, 'x_j3_o2_t4': 1, 'x_j3_o2_t5': 0,
+            'x_j3_o3_t3': 0, 'x_j3_o3_t4': 0, 'x_j3_o3_t5': 1, 'x_j3_o3_t6': 0
+        }]
+        for schedule in invalid_schedules:
+            self.assertFalse(csp.check(schedule))
